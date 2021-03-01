4411b3ae791b2b62d146819cddeb2ac835541703
DR2K <devdeeprajpal2k@outlook.com>
2021-03-01 09:46:51 -0600

    so many updates. in theory, leveling to google sheets works

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import linecache as ln
 import threading
 #for getting the time of a user's actions
 import time
+#random class
+import random as rd
 #for running bot using token see: "bot.run(config('TOKEN'))"
 from decouple import config
 #For all bot commands used and all data from discord API
@@ -17,6 +19,8 @@ from discord.ext import commands
 #
 #————————————————————————————————————————GLOBAL VARIABLES————————————————————————————————————————
 #
+
+restrict = ['TheTechnician#2981', 'C.J.#8161']
 
 #Prefix for commands in discord
 pre = '>'
@@ -33,11 +37,17 @@ ws = None
 #List of members who have spoken in the last minute, for "spam" protection
 members = []
 
-#Exp of Selected Author
-exp = None
+#ranks which have the privilage to be another role
+roleRanks = [5, 10, 20, 30, 40, 50, 60, 70]
 
 #Current row after update stats
 row = -1
+
+#point increment
+increment = rd.randint(25, 50)
+
+#cooldown between adding points
+cooldown = 60
 
 #
 #————————————————————————————————————————CLASSES————————————————————————————————————————
@@ -58,23 +68,40 @@ class kinder:
     def __str__(self):
         return 'The user, ' + self.id + 'is on cooldown from' + self.zeit
     
-
+#Class that defines the leveling for current level and if the author has level
 class level:
 
     def __init__(self, auth, xp, bot):
         self.auth = auth
-        self.xp = xp
+        self.xp = int(xp)
         self.level= 0
         self.ref = 'levelVals.txt'
         self.bot = bot
-        
+
     def get_level(self):
-        currentRank = 0
+        global ws, row
+        currentRank = self.get_orig_level()
+
+        if currentRank == 0:
+            for i in self.auth.roles:
+                if 'Rank' in str(i.name):
+                    currentRank = int(i.name[index(' ')+1:index(':')])
+                while int(ln.getline(self.ref, currentRank+1)) <= self.xp:
+                    currentRank+=1
+        else:
+            currentRank = int(currentRank)
 
         return currentRank
 
+    def get_orig_level(self):
+        orig = ws.cell(row, ws.row_values(1).index('Rank')+1).value
+        if orig is None:
+            return 0
+        return int(orig)
+
     def has_leveled(self):
-        if ln.getline('levelVals.txt', self.get_level()+1) <= self.xp:
+        nextLevelXP = int(ln.getline(self.ref, self.get_orig_level()+1))
+        if nextLevelXP <= self.xp:
             return True
         return False
 
@@ -88,7 +115,7 @@ class level:
 #Event that happens on initialization of bot
 @bot.event
 async def on_ready():
-    global ws, members
+    global ws, members, roleRanks
     #Prints to console that bot has logged in
     print('We have logged in as {0.user}'.format(bot))
 
@@ -102,22 +129,22 @@ async def on_ready():
 #Event that happens when a message is sent
 @bot.event
 async def on_message(message):
-    global auth, members, exp
+    global auth, members
     #Searches the members array, and if the author in one of the kinder objects, stops method.
     for kind in members:
         if kind.get_id() is auth:
             return
     
     #Makes sure the bot can't give itself points
-    if not str(message.author) in 'TheTechnician#2981':
+    if not str(message.author) in restrict:
         #Sets auth as the name of the person (DrDev#9289) as opposed to the object that message.author is
-        auth = str(message.author)
+        auth = message.author
 
         #Console recognition of adding one point to user auth's exp
-        print(f'added 1 point to {auth}\n')
+        print(f'added {increment} points to {auth}\n')
 
         #Discord recognition of adding one point to user auth's exp
-        await message.channel.send(f"one point added for {auth}")
+        await message.channel.send(f"{increment} points added for {auth}")
 
         #Creates a kinder object to add to members list, with the user's name. "spam" protection.
         members.append(kinder(auth))
@@ -144,7 +171,6 @@ async def say(ctx, *, par):
     #Console confirmation of this command working
     print(f'{auth} told TheTechnician to say {par}\n')
 
-
 #
 #————————————————————————————————————————HELPER METHODS————————————————————————————————————————
 #
@@ -164,7 +190,7 @@ async def check_time():
     while(True):
         #Checks all the objects in members list and if their time (in seconds) from creation is over 60, they're popped off the list.
         for kind in members:
-            if (int(time.time()) - kind.get_time()) > 60:
+            if (int(time.time()) - kind.get_time()) > cooldown:
                 members.pop()
         #Check every second (sleeps for one second)
         time.sleep(1)
@@ -173,7 +199,7 @@ async def check_time():
 async def update_stats():
     #Pay attention, you bot.
     await bot.wait_until_ready()
-    global auth, ws, exp, row
+    global auth, ws, row, roleRanks, increment
     
     #Find the name of the server the bot is in.
     serverName = bot.guilds[0].name
@@ -188,7 +214,7 @@ async def update_stats():
             break
 
     #Finds the row that the author of the message is in. Returns list
-    cell_list = ws.findall(auth)
+    cell_list = ws.findall(str(auth))
 
     #If there are no entries, time to create a row for the ~new~ user
     if len(cell_list) == 0:
@@ -197,49 +223,39 @@ async def update_stats():
         #Set row
         row = numPeople+1
         #Create the row for that user
-        ws.update_cell(row, 1, auth)
-        #Add the inital value for sending their first message, 25 exp
-        ws.update_cell(row, column, '25')
+        ws.update_cell(row, 1, str(auth))
+        #Add the inital value for sending their first message, {increment} exp
+        ws.update_cell(row, column, f'{increment}')
     else:
         #Grabs the location of the player's row. If done right, player never has more than one entry in the list
         row = cell_list[0].row
         try:
-            #Grabs value already in cell, and adds 25 to it.
-            newVal = int(ws.cell(row, column).value)+25
+            #Grabs value already in cell, and adds {increment} to it.
+            newVal = int(ws.cell(row, column).value)+increment
         except Exception:
-            #If no value in cell, the new value is 25 (one message)
-            newVal = 25
+            #If no value in cell, the new value is {increment} (one message)
+            newVal = increment
         
         #Update exp to newVal in server, denoted by column, for user, denoted by row
         ws.update_cell(row, column, newVal)
 
-        #levelCheck = level(message.author, exp, message)
-        #if levelCheck.has_leveled():
-        #    if(level.get_level() > )
+    col = ws.row_values(1).index('TOTALEN')+1
+    levelCheck = level(auth, ws.cell(row, col).value, bot)
+    if levelCheck.has_leveled():
+        ws.update_cell(row, ws.row_values(1).index('Rank')+1, levelCheck.get_level())
+        if levelCheck.get_level() in roleRanks:
+            await add_rank_role(level.get_level())
 
+async def add_rank_role(rank):
+    global auth, bot
+    roleName = f'Rank {rank}'
+    for role in bot.guilds[0].roles:
+        if roleName in role.name:
+            await bot.add_roles(auth, role.name)
 
-'''
-@bot.command()
-async def clearStats(ctx):
-    with open("stats.txt", 'w') as f:
-        f.write("")
-    await ctx.send("Cleared stats.txt!!")
-    print('stats cleared\n')
-
-
-@bot.command()
-async def stats(ctx):
-    with open("stats.txt", 'r') as f:
-        cont=f.read()
-    
-    try:	
-        await ctx.send(cont)
-        print('stats printed\n')
-
-    except Exception:
-        await ctx.send('`NO STATS PRESENT`')
-        print('no stats\n')
-'''
+        elif role.index(role.name) == (len(role)-1):
+            await bot.create_role(auth.server, name=roleName)
+            await bot.add_roles(auth, roleName)
 
 #
 #————————————————————————————————————————INITIALIZATION————————————————————————————————————————
