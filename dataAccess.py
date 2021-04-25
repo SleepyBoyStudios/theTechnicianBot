e1cc66adcf3ec0bef44e352b4969dde9962b8f97
eliteabhi <abhirangarajan2003@gmail.com>
2021-04-25 16:01:13 -0500

    did a clean of everything

--- dataAccess.py
+++ dataAccess.py
@@ -1,16 +1,42 @@
 #Data Storage
 import pandas as pd
 
-def addUser():
+def add_xp(id):
+    ids = pd.read_csv("data.csv")["ID"].tolist()
+    if id in ids:
+        #Add xp
+        return
+    
+    else:
+        addUser(id)
+
+def add_user(id):
+    data = {"ID": id, "XP": 0, "Time": 0}
+
+    df = pd.DataFrame(columns = ['ID', 'XP', 'Time'])
+    df = df.append(data, ignore_index=True)
+
+    print(df)
+
+
+def del_user():
     return
 
-def delUser():
+def edit_user():
     return
 
-def editUser():
-    return
+def grab_user_info(id):
+    df = pd.read_csv("data.csv")
 
-def grabUser(auth):
-    return
+    ids = df["ID"].tolist()
+
+    if id in ids:
+        return df["XP"].tolist()[ids.index(id)], df["Time"].tolist()[ids.index(id)]
+    
+    else:
+        add_user(id)
+        return False, False
+
+
 #def loadData()
 #def saveData()
