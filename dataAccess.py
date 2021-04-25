d463a78c9ffe419b13ff97947a29d0127da2d0bf
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-04-25 16:59:25 -0500

    cleaned up the constants and fixed compile errors

--- dataAccess.py
+++ dataAccess.py
@@ -1,23 +1,18 @@
 #Data Storage
 import pandas as pd
+import constants as const
 
-def add_xp(id):
-    ids = pd.read_csv("data.csv")["ID"].tolist()
-    if id in ids:
-        #Add xp
-        return
-    
-    else:
-        addUser(id)
+#constants
 
 def add_user(id):
     data = {"ID": id, "XP": 0, "Time": 0}
-
+    #make temporary DataFrame
     df = pd.DataFrame(columns = ['ID', 'XP', 'Time'])
     df = df.append(data, ignore_index=True)
 
     print(df)
-
+    #update csv file
+    df.to_csv(const.CSV_NAME, mode='a', index=False)
 
 def del_user():
     return
@@ -25,8 +20,17 @@ def del_user():
 def edit_user():
     return
 
+def add_xp(id):
+    ids = pd.read_csv(const.CSV_NAME)["ID"].tolist()
+    if id in ids:
+        #Add xp
+        return
+    
+    else:
+        add_user(id)
+
 def grab_user_info(id):
-    df = pd.read_csv("data.csv")
+    df = pd.read_csv(const.CSV_NAME)
 
     ids = df["ID"].tolist()
 
