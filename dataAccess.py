19641cfc8fc2e4725fdc281603d10681db877557
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-04-25 22:49:55 -0500

    Added add_xp Function

--- dataAccess.py
+++ dataAccess.py
@@ -1,8 +1,11 @@
 #Data Storage
 import pandas as pd
+from pandas.core.algorithms import mode
+from pandas.core.indexes.base import Index
 from constants import CSV_NAME
+import random as rd
 
-#constants
+
 
 def add_user(id):
     data = {"ID": id, "XP": 0, "Time": 0}
@@ -12,7 +15,9 @@ def add_user(id):
 
     print(df)
     #update csv file
-    df.to_csv(CSV_NAME, mode='a', index=False)
+    save_data(df)
+
+
 
 def del_user(id):
     df = pd.read_csv(CSV_NAME)
@@ -24,22 +29,26 @@ def del_user(id):
     print(df)
 
     print("Dropping id: " + str(id) + "...")
-    df.to_csv(CSV_NAME, mode='w', index=False)
+    save_data(df)
     print("Done!\n")
 
 
 
-def edit_user():
-    return
-
 def add_xp(id):
-    ids = pd.read_csv(CSV_NAME)["ID"].tolist()
+    df = load_data()
+    ids = df["ID"].tolist()
     if id in ids:
         #Add xp
-        return
+        xp = df["XP"].get(ids.index(id)) + rd.randint(25, 50)
+        df["XP"] = df["XP"].replace(to_replace= df["XP"].get(ids.index(id)), value=xp)
+        
+        #Update CSV
+        save_data(df)
     
     else:
         add_user(id)
+
+
 
 def grab_user_info(id):
     df = pd.read_csv(CSV_NAME)
@@ -54,5 +63,10 @@ def grab_user_info(id):
         return False, False
 
 
-#def loadData()
-#def saveData()
+
+def load_data():
+    return pd.read_csv(CSV_NAME)
+
+
+def save_data(new_df):
+    new_df.to_csv(CSV_NAME, mode='w', index=False)
