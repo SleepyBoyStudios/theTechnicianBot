78ae5df2aad0474c81cc643cec64830bfdc90b29
Devdeep Rajpal <devdeeprajpal2k@outlook.com>
2021-05-03 21:31:25 -0500

    Formatting refactor. Now consistent across all files. *Camel case*

--- creation.py
+++ creation.py
@@ -1,23 +1,23 @@
-#Data Storage
+# Data Storage
 import pandas as pd
 from constants import CSV_NAME
 
-df = pd.DataFrame(columns = ['ID', 'XP', 'Time'])
-df = df.append({"ID" : 330075847799341076, "XP" : -1, "Time" : 100}, ignore_index=True)
-df = df.append({"ID" : 190625917507207171, "XP" : 5000, "Time" : 101}, ignore_index=True)
-df = df.append({"ID" : 297540904133197826, "XP" : 123, "Time" : 102}, ignore_index=True)
-df = df.append({"ID" : 684395467722850345, "XP" : 3425, "Time" : 103}, ignore_index=True)
+df = pd.DataFrame(columns=['ID', 'XP', 'Time'])
+df = df.append({"ID": 330075847799341076, "XP": -1, "Time": 100}, ignore_index=True)
+df = df.append({"ID": 190625917507207171, "XP": 5000, "Time": 101}, ignore_index=True)
+df = df.append({"ID": 297540904133197826, "XP": 123, "Time": 102}, ignore_index=True)
+df = df.append({"ID": 684395467722850345, "XP": 3425, "Time": 103}, ignore_index=True)
 
-#print(df)
-df.to_csv(CSV_NAME, mode='w', index=False) #mode='a' for append
+# print(df)
+df.to_csv(CSV_NAME, mode='w', index=False)  # mode='a' for append
 
-df=pd.DataFrame()
+df = pd.DataFrame()
 
-df=pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
 
 ids = df["ID"].tolist()
 xps = df["XP"].tolist()
 print(ids)
 print(xps)
 
-#print(df)
+# print(df)
--- main.py
+++ main.py
@@ -1,107 +1,100 @@
-import asyncio
-#discord API
-import discord
-#for getting the time of a user's actions
-import time
-#random class
-import random as rd
-#for running bot using token see: "bot.run(config('TOKEN'))"
+# for running bot using token see: "bot.run(config('TOKEN'))"
 from decouple import config
-#For all bot commands used and all data from discord API
+# For all bot commands used and all data from discord API
 from discord.ext import commands
-#Data Storage
-import pandas as pd
-#Import other files
+# Import other files
 import logic as lg
-#Access the user data
-import dataAccess as da
-import time
+# Access the user data
+import data_access as da
 
 restrict = []
 
-bot = commands.Bot('>') #Initializes bot with '>' prefix
+bot = commands.Bot('>')  # Initializes bot with '>' prefix
 
 
-#On bot startup...
+# On bot startup...
 @bot.event
 async def on_ready():
     print("bot is oPeRAtiOnaL")
 
 
-#On each message...
+# On each message...
 @bot.event
 async def on_message(message):
     global restrict
-    if (message.author == bot.user) or (message.author in restrict): #Check if the user is the bot or is in the restrict list 
+    # Check if the user is the bot or is in the restrict list
+    if (message.author == bot.user) or (message.author in restrict):
         return
-    
-    #Sets auth as the name of the person in ID form
+
+    # Sets auth as the name of the person in ID form
     auth = message.author.id
 
-    #Check if message sender is allowed to accrue points
-    if(lg.deny_check(auth, restrict)):
+    # Check if message sender is allowed to accrue points
+    if lg.deny_check(auth, restrict):
         return
-    
+
     da.add_xp(auth)
 
-#-------------------------------------------------------------------------- COMMANDS --------------------------------------------------------------------------
 
-#--------------------------- ADMIN ONLY ---------------------------
+# ------------------------------------------------------ COMMANDS ------------------------------------------------------
 
-#Adds player to restrict list
+# --------------------------- ADMIN ONLY ---------------------------
+
+# Adds player to restrict list
 @bot.command
 async def restrict(auth):
     return
 
 
-#Removes player from restrict list
+# Removes player from restrict list
 @bot.command
 async def unrestrict(auth):
     return
 
 
-#Adds xp to a player
+# Adds xp to a player
 @bot.command
-async def addXp(auth):
+async def add_xp(auth):
     return
 
 
-#Removes xp from a player
+# Removes xp from a player
 @bot.command
-async def removeXp(auth):
+async def remove_xp(auth):
     return
 
 
-#Level up player
+# Level up player
 @bot.command
-async def lvlUp(auth):
+async def lvl_up(auth):
     return
 
 
-#Level down a player
+# Level down a player
 @bot.command
-async def lvlDown(auth):
+async def lvl_down(auth):
     return
 
 
-#Level a player to a rank
+# Level a player to a rank
 @bot.command
-async def lvlTo(auth, rank):
+async def lvl_to(auth, rank):
     return
 
 
-#Prints out the DataFrame
+# Prints out the DataFrame
 @bot.command
 async def df():
     return
 
 
-#Prints out the CSV
+# Prints out the CSV
 @bot.command
 async def csv():
     return
 
-#--------------------------- GENERAL ---------------------------
 
-#makes sure bot runs with the token from the .env file
+# --------------------------- GENERAL ---------------------------
+
+# makes sure bot runs with the token from the .env file
 bot.run(config('TOKEN'))
