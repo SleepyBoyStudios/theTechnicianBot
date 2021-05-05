daf5bd995c2842e65e1f1aecfad4aa1658afcfbc
Abhi Rangarajan <abhirangarajan2003@gmail.com>
2021-05-05 10:47:01 -0500

    updated gitignore because data.csv is required for pytest

--- constants.py
+++
@@ -1,3 +0,0 @@
-CSV_NAME = 'data.csv'
-LVL_LIST = 'levelVals.csv'
-RANK_DICT = {"Rank 1: Silver": 10, "Rank 2: Gold": 20, "Rank 3: Diamond": 30, "Rank 4: Platinum": 50, "Rank 5: Beskar": 100, "Rank 6: Mandalorian": 200, "Rank 7: Jedi Master": 350}
