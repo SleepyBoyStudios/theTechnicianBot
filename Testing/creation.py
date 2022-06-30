c39936891028883ef52d74145c759ed2520ecc00
Devdeep Singh Rajpal <devdeep@umich.edu>
2022-06-29 22:55:41 -0500

    added package to facilitate sqlite to pandas dataframe

--- Testing/creation.py
+++ Testing/creation.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import os
 
-# TODO: Find a way to handle a dict in a database, as of rn I have no clue 
+# TODO: Find a way to handle a dict in a database, as of rn I have no clue
 
 # df = pd.DataFrame(columns=["ID", "XP", "Time", "Lvl"])
 # df = df.append({"ID": "330075847799341076", "XP": {"ID": "330075847799341076", "810002138830471178": -1, "gaming": 1, "arts": 1, "fps": 1, "sl": 1, "dnd": 1}, "Time": 100, "Lvl": 0}, ignore_index=True)
@@ -16,9 +16,8 @@ import os
 # df = df.append({"ID": "553441706318626856", "XP": {"ID": "553441706318626856", "810002138830471178": 5, "gaming": 284, "arts": 284, "fps": 284, "sl": 284, "dnd": 284}, "Time": 103, "Lvl": 9}, ignore_index=True)
 
 # sqldf = df.to_sql('data',con=sqlite3.connect('data.db'), if_exists='replace'
-os.chdir("/home/abhi/Documents/Code-local/Python/theTechnicianBot/")
 
-conn = db.connect('./../data.db')
+conn = db.connect('./Testing/data.db')
 
 query = conn.cursor()
 
@@ -52,31 +51,35 @@ CREATE TABLE Server_810002138830471178 (
 # if (query.fetchall()) is None:
 # query.executescript(sql_script)
 
-db.register_adapter(np.int64, int) #! tried adding this but it didnt work
+db.register_adapter(np.int64, int)  #! tried adding this but it didnt work
 
 df = pd.read_csv(r"./Testing/TestDataCSV/User_info.csv")
 
 print(f'DataFrame: {type(df["user_ID"][0])}')
 
-df.to_sql(name = 'User_Info', con = conn, if_exists = 'append', index = False)
+df.to_sql(name='User_Info', con=conn, if_exists='append', index=False)
 
 # ---------------------------------------------------------------
 
 df = pd.read_csv(r"./Testing/TestDataCSV/server_1.csv")
 
-df.to_sql(name = 'Server_991178883682541700', con = conn, if_exists = 'append', index = False)
+df.to_sql(name='Server_991178883682541700',
+          con=conn,
+          if_exists='append',
+          index=False)
 
 # ---------------------------------------------------------------
 
 df = pd.read_csv(r"./Testing/TestDataCSV/server_2.csv")
 
-df.to_sql(name = 'Server_810002138830471178', con = conn, if_exists = 'append', index = False)
+df.to_sql(name='Server_810002138830471178',
+          con=conn,
+          if_exists='append',
+          index=False)
 
 # ---------------------------------------------------------------
 
 conn.commit()
-
-conn.close()
 
 df = pd.DataFrame()
 
@@ -85,4 +88,6 @@ df = pd.read_sql_table('User_Info', conn)
 ids = df["ID"].tolist()
 xps = df["XP"].tolist()
 
-print("Sample database created",'\n')
+conn.close()
+
+print("Sample database created", '\n')
